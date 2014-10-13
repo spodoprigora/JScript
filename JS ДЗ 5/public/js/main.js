f75b938f04815a3833d851e8cef83cae1bbb4576
spodoprigora <slivers@ukr.net>
2014-10-13 18:24:19 +0300

    add_5

---
+++ JS ДЗ 5/public/js/main.js
@@ -0,0 +1,14 @@
+﻿//ДЗ лекция 5 
+//Реализовать сценарий, выполняющий переадресацию пользователя на случайно
+// выбранный из массива адрес. Массив должен хранить не менее 10 адресов. 
+//Для каждого адреса напишите html страницу с рекламным объявлением. 
+
+
+var arr =new Array("advertisement1.html", "advertisement2.html", "advertisement3.html",
+                     "advertisement4.html", "advertisement5.html", "advertisement6.html", 
+                     "advertisement7.html", "advertisement8.html", "advertisement9.html", "advertisement10.html" );
+   
+function newpage(){
+    var i = Math.floor(Math.random()*10);
+    location.href =arr[i];	
+	}
